40e24f32728af5e4c646f504030fb0b5343c3b8c
Julio <juliofornode@gmail.com>
2015-08-11 13:51:57 -0700

    second commit

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
 
 var app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
+app.get('/contactlist', function (req, res) {
+  console.log('I received a GET request');
+
+  db.contactlist.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+app.post('/contactlist', function (req, res) {
+  console.log(req.body);
+  db.contactlist.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
 
 
 app.listen(3000);
