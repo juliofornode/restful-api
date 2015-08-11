507ed6828a1274c7682d81df4d04338897c2d1db
Julio <juliofornode@gmail.com>
2015-08-11 14:25:47 -0700

    third commit

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -9,14 +9,11 @@ myApp.controller('AppCtrl',
     '$scope',
     '$http',
     function($scope, $http) {
-      console.log("Hello World from controller");
-    }
-  ]
-);
+
+
 
 var refresh = function() {
   $http.get('/contactlist').success(function(response) {
-    console.log("I got the data I requested");
     $scope.contactlist = response;
     $scope.contact = "";
   });
@@ -55,6 +52,8 @@ $scope.update = function() {
 
 $scope.deselect = function() {
   $scope.contact = "";
-}
+};
 
-}]);﻿
+    }
+  ]
+);
